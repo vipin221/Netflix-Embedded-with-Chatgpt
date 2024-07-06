fd2b62744ebe1ad0716307ddb90d72bf7481e36d
Vipin Maurya <mauryavipin30@gmail.com>
2024-07-06 20:58:44 +0530

    updated

--- src/utils/openai.js
+++ src/utils/openai.js
@@ -1,8 +1,7 @@
 import OpenAI from 'openai';
-import { OPENAI_KEY } from './constant';
 
 const openai = new OpenAI({
-    apiKey: OPENAI_KEY,
+    apiKey: process.env.REACT_APP_API_KEY,
     dangerouslyAllowBrowser: true,
     
 });
